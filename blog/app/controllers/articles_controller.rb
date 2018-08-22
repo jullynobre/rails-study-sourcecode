8defc1cd6b758d0e052b522e36b9ef2c3064e33d
jullynobre <jullynobre1@gmail.com>
2018-08-22 00:05:08 -0300

    Upgrade rails; show

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,6 +1,20 @@
 class ArticlesController < ApplicationController
-
-    def new
+    def show
+        @article = Article.find(params[:id])
     end
     
+    def new
+    end
+
+    def create
+        @article = Article.new(article_params)
+       
+        @article.save
+        redirect_to @article
+      end
+       
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
 end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_22_024852) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
